8c861fbe32ec6d4420d2cf100bb8f2e942295deb
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-05-13 14:32:14 -0300

    feat: Added EsNExt examples: promises, spread and rest, arrow functions and new OO implementation.

---
+++ ESNext/revisao1.js
@@ -0,0 +1,23 @@
+// Let and const
+
+{
+    var a = 2;
+    let b = 3;
+    console.log(b);
+}
+
+console.log(a);
+
+// Template String
+const produto = 'iPad';
+console.log(`${produto} é caro!`);
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r";
+
+console.log(l, e, tras);
+
+const [x, , y] = [1, 2, 3];
+console.log(x, y);
+
+const { idade, nome } = { nome: 'Ana', idade: 0 };
